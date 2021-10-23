f07983a149e0c4f84cc70ec81ed58f59ba97e5d5
Javaluca <g.pellegrini89@gmail.com>
2021-10-23 23:35:26 +0200

    Fix on predicate dictionary

--- shift-engine/src/main/java/com/novihub/shift/predicate/DictionaryPredicate.java
+++ shift-engine/src/main/java/com/novihub/shift/predicate/DictionaryPredicate.java
@@ -1,15 +1,28 @@
 package com.novihub.shift.predicate;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class DictionaryPredicate {
 	
-	public static Method supplyPredicateFactory(String methodName, List<String> arguments) {
-		final Method[] metodi = DayPredicate.class.getDeclaredMethods();
+	static Method[] methods;
+	
+	static {
 		
-		return Arrays.stream(metodi)
+		Collection<Method> collection = new ArrayList<Method>();
+		collection.addAll(Arrays.asList(DayPredicate.class.getDeclaredMethods()));
+		collection.addAll(Arrays.asList(WeekPredicate.class.getDeclaredMethods()));
+		collection.addAll(Arrays.asList(MonthPredicate.class.getDeclaredMethods()));
+
+		methods = collection.toArray(new Method[] {});
+	}
+	
+	public static Method supplyPredicateFactory(String methodName, List<String> arguments) {
+		return Arrays.stream(methods)
+			.filter(m -> m.getName().equals(methodName))
 			.filter(m -> m.getParameterCount() == arguments.size())
 			.findAny()
 			.orElseThrow(() -> new RuntimeException("Predicate factory not found"));
